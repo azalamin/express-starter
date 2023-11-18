9ce2ac60c43dfd8733fc4cf334805f9c7cf5bf5a
Al Amin Sheikh <azalamins@gmail.com>
2023-11-19 04:31:06 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+const app = express();
+
+// parsers
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+	res.send("Hello World, How's going!");
+});
+
+app.post("/", (req: Request, res: Response) => {
+	console.log(req.body);
+	res.send("got data");
+});
+
+export default app;
