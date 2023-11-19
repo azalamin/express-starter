80cacf451b5f8a56c87ebd746198cfa86060d12a
Al Amin Sheikh <azalamins@gmail.com>
2023-11-19 15:16:11 +0600

    error handling done

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -8,18 +17,53 @@ const app = (0, express_1.default)();
 // parsers
 app.use(express_1.default.json());
 app.use(express_1.default.text());
+// Routing
+const userRouter = express_1.default.Router();
+const courseRouter = express_1.default.Router();
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/courses", courseRouter);
 const logger = (req, res, next) => {
-    console.log(req.url, req.method, req.hostname);
+    // console.log(req.url, req.method, req.hostname);
     next();
 };
-app.get("/", logger, (req, res) => {
-    console.log(req.query);
-    res.send("Hello World, How's going!");
-});
-app.post("/", logger, (req, res) => {
-    console.log(req.body);
+courseRouter.post("/create-course", (req, res) => {
+    const user = req.body;
+    console.log(user);
     res.json({
-        message: "Posted",
+        success: true,
+        message: "User created successfully!",
+        data: user,
     });
+});
+userRouter.get("/", logger, (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        res.send(something);
+    }
+    catch (error) {
+        next(error);
+    }
+}));
+app.post("/", logger, (req, res) => {
+    const user = req.body;
+    res.json({
+        success: true,
+        message: "User is created successfully!",
+        data: user,
+    });
+});
+// global error handler
+app.all("*", (req, res) => {
+    res.status(400).json({
+        success: false,
+        message: "Route not found",
+    });
+});
+app.use((error, req, res, next) => {
+    if (error) {
+        res.status(400).json({
+            success: false,
+            message: "Something went wrong!",
+        });
+    }
 });
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -5,22 +5,61 @@ const app = express();
 app.use(express.json());
 app.use(express.text());
 
-const logger = (req: Request, res: Response, next: NextFunction) => {
-	console.log(req.url, req.method, req.hostname);
+// Routing
+const userRouter = express.Router();
+const courseRouter = express.Router();
 
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/courses", courseRouter);
+
+const logger = (req: Request, res: Response, next: NextFunction) => {
+	// console.log(req.url, req.method, req.hostname);
 	next();
 };
 
-app.get("/", logger, (req: Request, res: Response) => {
-	console.log(req.query);
-	res.send("Hello World, How's going!");
+courseRouter.post("/create-course", (req: Request, res: Response) => {
+	const user = req.body;
+	console.log(user);
+
+	res.json({
+		success: true,
+		message: "User created successfully!",
+		data: user,
+	});
+});
+
+userRouter.get("/", logger, async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		res.send(something);
+	} catch (error) {
+		next(error);
+	}
 });
 
 app.post("/", logger, (req: Request, res: Response) => {
-	console.log(req.body);
+	const user = req.body;
 	res.json({
-		message: "Posted",
+		success: true,
+		message: "User is created successfully!",
+		data: user,
 	});
+});
+
+// global error handler
+app.all("*", (req: Request, res: Response) => {
+	res.status(400).json({
+		success: false,
+		message: "Route not found",
+	});
+});
+
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+	if (error) {
+		res.status(400).json({
+			success: false,
+			message: "Something went wrong!",
+		});
+	}
 });
 
 export default app;
